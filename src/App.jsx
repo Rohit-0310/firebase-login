598ecc1eb22781c81e7cee42af28bb5b615d1d70
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-05-20 23:00:37 +0530

    not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
     return (
         <div className="App">
-            <h1>App.jsx</h1>
+            <Login />
         </div>
     );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgtUbrI8WglM0T2aKjwo_qbMFsCQM4A0c",
+  authDomain: "react-authentication-67322.firebaseapp.com",
+  projectId: "react-authentication-67322",
+  storageBucket: "react-authentication-67322.appspot.com",
+  messagingSenderId: "248415278971",
+  appId: "1:248415278971:web:a6d05394ef7298b46dcfde",
+  measurementId: "G-HJ41QY4DLK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
